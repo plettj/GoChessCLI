4d8180bff099b980438d80e314e69a2eb3d98282
Josiah Plett <jlplett303@gmail.com>
2022-06-15 22:22:16 -0700

    ... the beauty of the world in which you live   - Josiah, 2022

--- printBoard.go
+++ printBoard.go
@@ -10,6 +10,8 @@ import (
 
 // regex checking the fen validity: \s*([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw-]\s(([a-hkqA-HKQ]{1,4})|(-))\s(([a-h][36])|(-))\s\d+\s\d+\s*
 
+const asciiPieces = true
+
 var pieces = map[string]string {
 	"K": "♔",
 	"Q": "♕",
@@ -25,6 +27,7 @@ var pieces = map[string]string {
 	"p": "♟",
 }
 
+
 // Takes a FEN string; returns whether it's formed properly
 func validateFEN(boardState string) bool {
   re := regexp.MustCompile(`\s*([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw-]\s(([a-hkqA-HKQ]{1,4})|(-))\s(([a-h][36])|(-))\s\d+\s\d+\s*`)
@@ -37,13 +40,41 @@ func printFEN(boardState string) {
     return
   }
 
-  values := strings.Split(boardState, " ")
-	fmt.Printf("\nPrinting board from: %v", values[3])
-  fmt.Printf(pieces["K"])
+  // [board, toMove, castling, epSquare, halfmoveClock, moveNum]
+  fen := strings.Split(boardState, " ")
+  var extraStuff = [2]string {"", ""}
+  if asciiPieces {
+    extraStuff[0] = " "
+    extraStuff[1] = "═"
+  }
 
-}
+  var rank = 8;
+  var turnMessage = "White to move."
+  if (fen[1] == "b") {
+    turnMessage = "Black to move."
+  }
+  var castlingPermissions = "Castling Permissions: " + fen[2]
+  var rankMessages = [8]string {"", "", "", "", "", "", "", turnMessage}
 
-// Takes a PGN string; prints the board accordingly
-func printPGN(boardPGN string) {
+  fmt.Printf("\n  ╔%v════════════════════════╗\n8 ║", extraStuff[1])
+  for _, char := range fen[0] {
+    if (char == '/') {
+      rank--
+      fmt.Printf("%v║  %v\n%v ║", extraStuff[0], rankMessages[rank], rank)
+    } else if piece, isPiece := pieces[string(char)]; isPiece {
+      if (asciiPieces) {
+        fmt.Print(" ", piece, " ")
+      } else {
+        fmt.Print(" ", string(char), " ")
+      }
+    } else {
+      for i:= 0; i < int(char - '0'); i++ {
+        fmt.Print(" · ")
+      }
+    }
+  }
+  fmt.Printf("%v║  %v\n  ╚%v════════════════════════╝\n    a  b  c  d  e  f  g  h\n", extraStuff[0], castlingPermissions, extraStuff[1])
+
+	fmt.Print("\nPrinting board from this FEN:\n", boardState, "\n\n")
 
 }
